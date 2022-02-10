ab410aa09a356bb3e44e40406ab9774c4efa616c
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-10 21:14:43 +0700

    define multiply function

---
+++ fun_05_multiply.py
@@ -0,0 +1,16 @@
+def multiply(x: float, y: float) -> float:
+    """
+    Multiply 2 numbers.
+
+    Although function is designed to multiply two numbers,
+    can multiply a sequence. If you pass a string as `x`,
+    it will be repeated `y` times, as returned value.
+
+    :param x:
+    :param y:
+    :return: product of `x` and `y`.
+    """
+
+    result = x * y
+    return result
+

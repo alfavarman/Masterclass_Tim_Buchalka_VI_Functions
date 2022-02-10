c8565d0efd123339b1286f7353cbb02d886b1627
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-10 21:08:32 +0700

    function handle negative input

--- fun_04_fibonacci_number.py
+++ fun_04_fibonacci_number.py
@@ -1,6 +1,6 @@
 def fibonacci_numbers(n=100):
     """
-    provide fibonacci numbers for n' range.
+    return fibonacci numbers for n' range.
     n must be positive int. default n is 100.
     NOTE fibonacci n = 10 is 55. N is range of indexes.
     :param n is range for which fibonacci numbers are calculated
@@ -10,10 +10,14 @@ def fibonacci_numbers(n=100):
         return n
 
     n_minus1,n_minus2, = 1, 0
+    fn = None
     for f in range(n -1):
         fn = n_minus1 + n_minus2
         print(fn)
         n_minus2 = n_minus1
         n_minus1 = fn
+
+    return fn
+
 
 print(fibonacci_numbers(10))

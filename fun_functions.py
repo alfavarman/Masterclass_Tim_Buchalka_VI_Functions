65b1443abe77f77e8289aab935e294b1bf516f24
Charles Karol Szatkowski <kaszatus@gmail.com>
2022-02-11 19:33:21 +0700

    factorial function added

--- fun_functions.py
+++ fun_functions.py
@@ -83,3 +83,18 @@ def is_palindrome_sentence(string: str) -> bool:
     return sent[::-1].casefold() == sent.casefold()
 
 
+def factorial(number: int) -> int:
+    """
+    Factorial is multiply of next number up to argument number
+
+    :param number: int
+    :return: int factorial for argument
+    """
+    if number == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, number+1):
+            result = result +(result * i)
+        return result
+
